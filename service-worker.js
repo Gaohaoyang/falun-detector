3fd6c7eed5eda69dbc9bc7a557dacfa0f3dd43f4
Haoyang Gao <gaohaoyang126@126.com>
2023-12-31 12:31:32 -0500

    feat: 🎸 rm comments

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log('first line detector: tab updated', tabId, changeInfo, tab)
   if (changeInfo.status === 'complete') {
     chrome.tabs.sendMessage(tabId, {
       message: 'tabUpdatedSendFromFLDetector',
